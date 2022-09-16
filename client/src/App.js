cc4580811343bb7a2e1671abccaae64c4fc8765f
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-09-16 14:08:17 -0500

    create basic NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Board from './components/Board';
 import CreateTask from './components/CreateTask';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route element={ <Board /> } path="/" />
           <Route element={ <CreateTask /> } path= "/create" />
