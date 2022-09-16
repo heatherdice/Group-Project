ed86171c3c260acf063187412743c9341ab9fbac
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2022-09-15 20:24:57 -0500

    create basic create task form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import CreateTask from './components/CreateTask';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={ <Home /> } path="/" />
+          <Route element = { <CreateTask /> } path="/create" />
         </Routes>
       </BrowserRouter>
     </div>
