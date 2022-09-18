830d8ac0ce05942a00e78b8c728427179317640b
Ed Fenton <ed.fenton@mac.com>
2022-09-18 17:31:36 -0500

    removing un-needed View ande Edit Members components and updating App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import CreateTask from './components/CreateTask';
 import NavBar from './components/NavBar';
-import ViewMember from './components/ViewMember';
 import UpdateTask from './components/UpdateTask';
 import ViewTask from './components/ViewTask';
 import './App.css';
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route element={ <Home /> } path="/" />
           <Route element={ <CreateTask /> } path= "/create" />
-          <Route element={ <ViewMember /> } path= "/member/:id" />
           <Route element={ <UpdateTask /> } path= "/updatetask/:id" />
           <Route element={ <ViewTask /> } path= "/task/:id" />
         </Routes>
