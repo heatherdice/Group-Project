8706fe572cbeac5b912ffca06d222febd3ac35ae
Ed Fenton <edfenton@users.noreply.github.com>
2022-09-18 14:13:31 -0500

    moved member initials from model to frontend and removed middleware

--- server/models/member.model.js
+++ server/models/member.model.js
@@ -7,17 +7,9 @@ const MemberSchema = new mongoose.Schema(
       required: [true, "Name is required"],
       minLength: [3, "Name must be at least 3 characters long"],
       maxLength: [255, "Name must be at most 255 characters long"],
-    },
-    initials: {
-      type: String,
     }
   },
   { timestamps: true }
 );
-
-MemberSchema.pre('save', function(next) {
-  this.initials = this.name.match(/\b([A-Za-z0-9])/g).join('').toUpperCase();
-  next();
-});
 
 module.exports = mongoose.model('Member', MemberSchema);
