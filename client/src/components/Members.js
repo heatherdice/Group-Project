427f505b3662c99b33e34435663d04e6a57278b5
Ed Fenton <edfenton@users.noreply.github.com>
2022-09-17 22:31:56 -0500

    finished the modals

--- client/src/components/Members.js
+++ client/src/components/Members.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 const Members = () => {
 
   // Creating State to hold the list of all members from the server.
-  const [members, setMembers] = useState([])
+  const [ members, setMembers ] = useState([])
+  const [ singleMember, setSingleMember ] = useState({});
 
-  // Create State to hold values for forms
+  // Create State to hold values for create form
   const [ name, setName ] = useState("");
   const [ errors, setErrors ] = useState([]);
 
@@ -21,11 +21,55 @@ const Members = () => {
     .catch(err => console.log(err))
   }, []);
 
+  const viewMemberHandler = (id) => {
+    axios
+      .get(`http://localhost:8000/api/members/${id}`)  
+      .then((res) => {
+        console.log(res.data);
+        setSingleMember(res.data);
+        setName(res.data.name)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+    // Delete handler for the view member modal
+    const deleteHandler = () => {
+      axios
+        .delete(`http://localhost:8000/api/members/${singleMember._id}`)
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+          setSingleMember({});
+          setName("");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  
+  // Update handler for the update member modal
+  const updateHandler = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:8000/api/members/${singleMember._id}`, { name })
+      .then((res) => {
+        console.log(res.data);
+        setSingleMember({});
+        setName("");
+      })
+      .catch((err) => {
+        console.log(err.response.data.errors);
+        setErrors(err.response.data.errors);
+      });
+  };
+
   // Clearing the form state when the modal is called
   const cleanUpHandler = (e) => {
     setName("");
     setErrors([]);
-  }
+  };
 
   // Submit handler for the create member modal
   const createHandler = (e) => {
@@ -45,25 +89,36 @@ const Members = () => {
 
     return (
       <div className="container">
-        <div className="row">
+        <div className="row d-flex align-items-baseline">
           <div className="col col-sm-3">
             <p className="h2">Group 5 Board</p>
           </div>
-          <div className="col col-sm-9">
+          <div className="col col-sm-1">
+            <p>Members:</p>
+          </div>
+          <div className="col">
             { members.map((oneMember, index) => {
               return (
-                <Link key={ index } className="m-1" to={ `/member/${ oneMember._id}` }>{ oneMember.initials }</Link>
+                // Button trigger view modal
+                <button
+                  key={ index }
+                  type="button"
+                  className="btn btn-link"
+                  data-bs-toggle="modal"
+                  data-bs-target="#viewMemberModal"
+                  onClick={ (e) => viewMemberHandler(`${oneMember._id}`) }
+                >{ oneMember.initials }</button>
               );
             })}
             {/* Button trigger create modal */}
             <button
               type="button"
-              className="member-add"
+              className="btn btn-link"
               data-bs-toggle="modal"
               data-bs-target="#createMemberModal"
               onClick={ cleanUpHandler }
             >
-              +
+              <i className="bi bi-plus-circle-fill"></i>
             </button>
           </div>
         </div>
@@ -97,8 +152,83 @@ const Members = () => {
                     { errors.name ? <p className="text-danger">{ errors.name.message }</p> : null }
                   </div>
                   <div className="d-flex justify-content-end">
-                    <button type="button" className="btn btn-link" data-bs-dismiss="modal">Done Creating Member</button>
+                    <button type="button" className="btn btn-link" data-bs-dismiss="modal">Close</button>
                     <button type="submit" className="btn btn-primary" onClick={ createHandler }>Create Members</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* View modal */}
+        <div
+          className="modal fade"
+          id="viewMemberModal"
+          tabIndex="-1"
+          aria-labelledby="viewMemberModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <p className="h5 modal-title" id="viewMemberModalLabel">View { name }</p>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                  <div className="mb-3">
+                    <p className="form-label">Member Name</p>
+                    <div className="d-flex gap-3 align-items-baseline">
+                      <p className="form-control bg-light">{ name }</p>
+                      {/* Button to trigger edit modal */}
+                      <button
+                        type="button"
+                        className="btn btn-link"
+                        data-bs-toggle="modal"
+                        data-bs-target="#updateMemberModal"
+                      >
+                        <i className="bi bi-pencil-fill"></i>
+                      </button>
+                    </div>
+                  </div>
+                <div className="d-flex justify-content-end">
+                  <button type="button" className="btn btn-link" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" className="btn btn-danger" onClick={ deleteHandler }>Delete { name }</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Edit modal */}
+        <div
+          className="modal fade"
+          id="updateMemberModal"
+          tabIndex="-1"
+          aria-labelledby="updateMemberModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <p className="h5 modal-title" id="updateMemberModalLabel">Update { name }</p>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="member-name" className="col-form-label">Member Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="member-name"
+                      onChange={ (e) => setName(e.target.value) }
+                      name="name"
+                      value={ name }
+                    />
+                    { errors.name ? <p className="text-danger">{ errors.name.message }</p> : null }
+                  </div>
+                  <div className="d-flex justify-content-end">
+                    <button type="button" className="btn btn-link" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" className="btn btn-primary" onClick={ updateHandler }>Update { name }</button>
                   </div>
                 </form>
               </div>
