8e40b81cf19b34303de44de86b61d71c552e57df
Ed Fenton <ed.fenton@mac.com>
2022-09-19 00:58:38 -0500

    fixed alignment of footer and only having it show on the home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import CreateTask from './components/CreateTask';
 import NavBar from './components/NavBar';
 import UpdateTask from './components/UpdateTask';
 import ViewTask from './components/ViewTask';
-import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -24,7 +23,6 @@ function App() {
           <Route element={ <ViewTask /> } path= "/task/:id" />
         </Routes>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
